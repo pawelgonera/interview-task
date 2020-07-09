f7cc06e5ce2e6a77a7652acb68d97f651bf7f8b6
Paweł Gónera <pawelgonera1987@gmail.com>
2020-07-10 00:03:47 +0200

    init task

---
+++ src/main/java/pl/pawelgonera/atmotermtask/AtmotermTaskApplication.java
@@ -0,0 +1,13 @@
+package pl.pawelgonera.atmotermtask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtmotermTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtmotermTaskApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/pawelgonera/atmotermtask/AtmotermTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.pawelgonera.atmotermtask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtmotermTaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
